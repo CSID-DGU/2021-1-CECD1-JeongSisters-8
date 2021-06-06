f5f4921acaa2487076fff0dfaf76895c1793fb5f
Jiyeon Jeong <48276522+jiyeon1@users.noreply.github.com>
2021-06-07 00:18:38 +0900

    add manual

--- web/detect.py
+++ web/detect.py
@@ -1,12 +1,13 @@
 import numpy as np
 from collections import Counter, namedtuple
 import argparse
-import time, math
+import time
+import math
 import cv2
 import os
 from flask import Flask, request, Response, jsonify, render_template
 import jsonpickle
-#import binascii
+# import binascii
 import io as StringIO
 import base64
 from io import BytesIO
@@ -21,10 +22,31 @@ confthres = 0.01
 nmsthres = 0.1
 yolo_path = './'
 
+# ajax 통신 변수
+tem_message = "temporary"
+final_message = "prediction result"
+cloth_labels_per_laundry = []
+stain_cnt_per_laundry = []
+stain_area_per_laundry = []
+summary = []
+summaries = []
+
+# Summary= namedtuple("Summary", 'cloth, stain_cnt, stain_area')
+const_sec = 30
+STOP = False
+SECOND = 20  # 5초(8초??)
+
+Manual = [{
+    "key": 0,
+    "detergent": "중성세제 반컵 사용",
+    "temp": 30,
+    "washCycle": "헹굼 2회 + 탈수 중"
+}]
+
 
 def get_labels(labels_path):
     # load the COCO class labels our YOLO model was trained on
-    #labelsPath = os.path.sep.join([yolo_path, "yolo_v3/coco.names"])
+    # labelsPath = os.path.sep.join([yolo_path, "yolo_v3/coco.names"])
     lpath = os.path.sep.join([yolo_path, labels_path])
     LABELS = open(lpath).read().strip().split("\n")
     return LABELS
@@ -129,18 +151,18 @@ def get_predection(image, net, LABELS, COLORS):
 
     # ensure at least one detection exists
     labels = []
-    stain_cnt = 0 #한 프레임 내에 존재하는 stain 갯수
+    stain_cnt = 0  # 한 프레임 내에 존재하는 stain 갯수
     stain_area = 0
     if len(idxs) > 0:
         # loop over the indexes we are keeping
-        for i in idxs.flatten(): #다차원 배열(array)을 1차원 배열로 평평하게 펴주는 함수
+        for i in idxs.flatten():  # 다차원 배열(array)을 1차원 배열로 평평하게 펴주는 함수
             # extract the bounding box coordinates
-            (x, y) = (boxes[i][0], boxes[i][1]) #시작지점
-            (w, h) = (boxes[i][2], boxes[i][3]) #가로 세로 길이
+            (x, y) = (boxes[i][0], boxes[i][1])  # 시작지점
+            (w, h) = (boxes[i][2], boxes[i][3])  # 가로 세로 길이
 
             # draw a bounding box rectangle and label on the image
             # color = [int(c) for c in COLORS[classIDs[i]]]
-            color=[0,0,0]
+            color = [0, 0, 0]
 
             # w = math.ceil(w*1.8)
             # h = math.ceil(h*1.8)
@@ -152,46 +174,46 @@ def get_predection(image, net, LABELS, COLORS):
             # get dimensions of image
             labels.append(LABELS[classIDs[i]])
             dimensions = image.shape
-            
+
             # height, width, number of channels in image
             height = dimensions[0]
             width = dimensions[1]
 
-            if x<10 or y<10:
-                print(x,y)
+            if x < 10 or y < 10:
+                print(x, y)
                 y = 25
-            if height<h:
+            if height < h:
                 h = height-2*y
             cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-            
+
             text = "{}: {:.1f}".format(LABELS[classIDs[i]], confidences[i])
             if LABELS[classIDs[i]] == 'stain':
-                #putText함수는 \n을 구분해내지 못하기 때문에 수동적으로 처리해야한다. 
+                # putText함수는 \n을 구분해내지 못하기 때문에 수동적으로 처리해야한다.
                 stain_size = w*h
                 # print(stain_size)
-                stain_cnt += 1 
+                stain_cnt += 1
                 stain_area += stain_size
-                cv2.putText(image, str(stain_size), (x + w, y + h), #이미지, 출력문자, 출력위치
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color,2) #폰트, 폰트크기, 색상, 두께
-                
+                cv2.putText(image, str(stain_size), (x + w, y + h),  # 이미지, 출력문자, 출력위치
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)  # 폰트, 폰트크기, 색상, 두께
+
             else:
-                if LABELS[classIDs[i]] != 'Button': #옷 종류들을 담는다. 
+                if LABELS[classIDs[i]] != 'Button':  # 옷 종류들을 담는다.
                     cloth_labels_per_laundry.append(LABELS[classIDs[i]])
                 # print(boxes)
-                print(LABELS[classIDs[i]],confidences[i])
+                print(LABELS[classIDs[i]], confidences[i])
                 # print(classIDs)
                 # print(confidences[i])
-            
-            
+
             # channels = img.shape[2]
             # print(width,height)
-            cv2.putText(image, text, (x, y-5), #이미지, 출력문자, 출력위치
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color,2) #폰트, 폰트크기, 색상, 두께
-    
+            cv2.putText(image, text, (x, y-5),  # 이미지, 출력문자, 출력위치
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)  # 폰트, 폰트크기, 색상, 두께
+
     stain_cnt_per_laundry.append(stain_cnt)
     stain_area_per_laundry.append(stain_area)
-    print(labels, stain_cnt_per_laundry,stain_area_per_laundry)
-    return image,labels
+    print(labels, stain_cnt_per_laundry, stain_area_per_laundry)
+    return image, labels
+
 
 # labelsPath = "/"
 labelsPath = "data/obj.names"
@@ -207,11 +229,11 @@ app = Flask(__name__)
 # Bootstrap(app)
 
 '''
-1. 
+1.
 한 프레임에 대한 라벨들 -> labels(local)
 한 프레임에 대한 stain 갯수-> stain_cnt(local)
 한 프레임에 대한 stain 면적 합-> stain_area(local)
-다음 프레임 진행하기 전에 초기화 
+다음 프레임 진행하기 전에 초기화
 
 2.
 한 세탁물에 대해 검출된 옷 라벨들 -> cloth_labels_per_laundry(global 배열)
@@ -231,24 +253,12 @@ summary 객체 = [predicted_cloth_label, predicted_stain_cnt, predicted_stain_ar
 
 '''
 
-# ajax 통신 변수
-tem_message = "temporary"
-final_message = "prediction result"
-cloth_labels_per_laundry = []
-stain_cnt_per_laundry = [] 
-stain_area_per_laundry = [] 
-summary = []
-summaries = []
-# Summary= namedtuple("Summary", 'cloth, stain_cnt, stain_area')
-const_sec = 30
-STOP = False
-SECOND = 20 #5초(8초??)
-
 
 @app.route('/')
 def index():
     """Video streaming ."""
-    return render_template('index.html',resultReceived=sendResult())
+    return render_template('index.html', resultReceived=sendResult())
+
 
 @app.route('/video_feed')
 def video_feed():
@@ -257,16 +267,17 @@ def video_feed():
     return Response(gen(camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 def gen(camera):
     global tem_message
-    global cloth_labels_per_laundry,stain_cnt_per_laundry, stain_area_per_laundry
+    global cloth_labels_per_laundry, stain_cnt_per_laundry, stain_area_per_laundry
     """Video streaming generator function."""
     if not camera.isOpened():
         raise RuntimeError("Could not start camera")
 
     # vc = cv2.VideoCapture(0)
     count = 0
-    i=0
+    i = 0
     second = SECOND
     start = time.time()
     while not STOP:
@@ -278,48 +289,52 @@ def gen(camera):
         res, labels = get_predection(image, nets, Lables, Colors)
         image = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
         # if second%const_sec==0: #1초마다 한 번씩
-        cv2.imwrite(pic_name, image) #변환된 이미지나 동영상의 특정 프레임을 저장
+        cv2.imwrite(pic_name, image)  # 변환된 이미지나 동영상의 특정 프레임을 저장
         # cv2.imshow('output', image) #읽어들인 이미지 파일을 윈도우창에 보여줌
         tem_message = ', '.join(labels)
         # np_img = Image.fromarray(image)
         # img_encoded = image_to_byte_array(np_img)
         '''imwrite랑 yield가 같이 있어야 화면에서 보임.'''
         yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + open(pic_name, 'rb').read() + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + open(pic_name, 'rb').read() + b'\r\n')
         # time.sleep(3)
-        i+=1
-        second -=1 
-        
+        i += 1
+        second -= 1
+
         if second == 0:
             print("global clothes")
-            print(cloth_labels_per_laundry,stain_cnt_per_laundry,stain_area_per_laundry)
-            cloth_labels_per_laundry=[] #15초마다 한 번씩 reset
-            stain_cnt_per_laundry =[]
-            stain_area_per_laundry=[]
+            print(cloth_labels_per_laundry,
+                  stain_cnt_per_laundry, stain_area_per_laundry)
+            cloth_labels_per_laundry = []  # 15초마다 한 번씩 reset
+            stain_cnt_per_laundry = []
+            stain_area_per_laundry = []
             print("summary")
             print(summary)
-            print("[INFO] YOLO took {:.6f} seconds".format(time.time() - start))
+            print("[INFO] YOLO took {:.6f} seconds".format(
+                time.time() - start))
             second = SECOND
 
         if img is None:
             print("Empty Frame")
             time.sleep(0.1)
-            count+=1
+            count += 1
             if count < 3:
                 continue
-            else: 
+            else:
                 break
+
 
 @app.route("/predicted_result")
 def totalResult():
-    #세탁물의 옷 종류를 예측하기. 
+    # 세탁물의 옷 종류를 예측하기.
     global cloth_labels_per_laundry
     if not cloth_labels_per_laundry:
         predicted_cloth_label = " Nothing Detected "
 
     else:
         clothes = Counter(cloth_labels_per_laundry)
-        predicted_cloth_label = clothes.most_common()[0][0] #여러 결과 중에 가장 빈도 수가 높게 나타난 옷
+        # 여러 결과 중에 가장 빈도 수가 높게 나타난 옷
+        predicted_cloth_label = clothes.most_common()[0][0]
 
         stain_cnt = Counter(stain_cnt_per_laundry)
         predicted_stain_cnt = stain_cnt.most_common()[0][0]
@@ -327,49 +342,118 @@ def totalResult():
         stain_area = Counter(stain_area_per_laundry)
         predicted_stain_area = stain_area.most_common()[0][0]
         summaries.append(
-            {'cloth':predicted_cloth_label,'stain_cnt':predicted_stain_cnt, 'stain_area':predicted_stain_area}
+            {'cloth': predicted_cloth_label, 'stain_cnt': predicted_stain_cnt,
+                'stain_area': predicted_stain_area}
         )
         # s = Summary(predicted_cloth_label, predicted_stain_cnt, predicted_stain_area)
         print('here')
         print(summaries)
-        summary.append(predicted_cloth_label) #예측기록에 추가 
+        summary.append(predicted_cloth_label)  # 예측기록에 추가
 
     return predicted_cloth_label
 
-@app.route("/change_stop_flag")
+
+@app.route("/showManual")
+def showManual():
+    x = y = z = 0
+    total_stain_size_x = total_stain_size_y = total_stain_size_z = 0
+    set_stain = 10000
+    global Manual
+    global cloth_labels_per_laundry
+
+    # 코스 정하기
+    if len(cloth_labels_per_laundry) > 0:
+        for i in cloth_labels_per_laundry:
+            if cloth_labels_per_laundry[i]["cloth"] == 'Swimwear':
+                y += 1
+                total_stain_size_y += cloth_labels_per_laundry[i]["stain_area"]
+            elif cloth_labels_per_laundry[i]["cloth"] == 'Towel':
+                z += 1
+                total_stain_size_z += cloth_labels_per_laundry[i]["stain_area"]
+            else:
+                x += 1
+                total_stain_size_x += cloth_labels_per_laundry[i]["stain_area"]
+
+    if y != 0:
+        Manual.append({
+            "key": 1,
+            "detergent": "중성세제 반컵 사용, 염소계표백제 사용 금지",
+            "temp": 40,
+            "washCycle": "헹굼 2회 + 탈수 약 + 건조 금지"
+        })
+        if total_stain_size_y > set_stain:
+            if Manual[1]["key"] == 1:
+                Manual[1]["washCycle"] = "헹굼 3회 + 탈수 약 + 건조 금지 + 오염도 높음"
+    if z != 0:
+        Manual.append({
+            "key": 2,
+            "detergent": "중성세제 반컵 사용, 섬유유연제 & 염소계표백제 사용 금지",
+            "temp": 40,
+            "washCycle": "헹굼 2회 + 탈수 약 + 건조 금지"
+        })
+        if total_stain_size_z > set_stain:
+            if Manual[1]["key"] == 2:
+                Manual[1]["washCycle"] = "헹굼 3회 + 탈수 약 + 오염도 높음"
+            elif Manual[2]["key"] == 2:
+                Manual[2]["washCycle"] = "헹굼 3회 + 탈수 약 + 오염도 높음"
+    if x > 7:
+        Manual[0] = {
+            "key": 3,
+            "detergent": "중성세제 한컵 사용",
+            "temp": 40,
+            "washCycle": "헹굼 3회 + 탈수 중"
+        }
+        if total_stain_size_x > set_stain:
+            Manual[0]["washCycle"] = "헹굼 4회 + 탈수 중 + 오염도 높음"
+    elif x <= 7:
+        if total_stain_size_x > set_stain:
+            Manual[0]["washCycle"] = "헹굼 3회 + 탈수 중 + 오염도 높음"
+
+    print("manual::"+Manual)
+    return Manual
+
+
+@ app.route("/change_stop_flag")
 def change_stop_flag():
     global STOP
     STOP = True
     return jsonify(STOP)
 
-@app.route('/redrawTable')
+
+@ app.route('/redrawTable')
 def redrawTable():
     return jsonify(summaries)
 
+
 '''
-아래는 TEST CODES 
+아래는 TEST CODES
 '''
-@app.route('/test')
+
+
+@ app.route('/test')
 def test():
     """Video streaming ."""
     return render_template('test.html')
 
-@app.route('/totalResult2')
+
+@ app.route('/totalResult2')
 def totalResult2():
     global cloth_labels_per_laundry
-    summaries =[
-        {'cloth':'12', 
-        'stain_cnt':21,'stain_area':12}
+    summaries = [
+        {'cloth': '12',
+         'stain_cnt': 21, 'stain_area': 12}
     ]
-    summaries.append(  {'cloth':'qwqwq', 
-        'stain_cnt':21,'stain_area':12})
+    summaries.append({'cloth': 'qwqwq',
+                      'stain_cnt': 21, 'stain_area': 12})
     # summaries = [
     #     Summary(cloth='Jhon', stain_cnt=28, stain_area='남'), Summary(cloth='Ciln', stain_cnt=24, stain_area='여')
     # ]
     return jsonify(summaries)
 
 # ajax 통신 함수
-@app.route("/sendResult")
+
+
+@ app.route("/sendResult")
 def sendResult():
     global tem_message, final_message
 
@@ -382,18 +466,20 @@ def sendResult():
     return final_message
 
 # route http posts to this method
-@app.route('/test_files', methods=['POST'])
+
+
+@ app.route('/test_files', methods=['POST'])
 def test_files():
     # load our input image and grab its spatial dimensions
-    #image = cv2.imread("./test1.jpg")
+    # image = cv2.imread("./test1.jpg")
     output = str(request.files["output"].read().decode("utf-8"))
-    
+
     img = request.files["image"].read()
     img = Image.open(io.BytesIO(img))
     npimg = np.array(img)
     image = npimg.copy()
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    res,labels = get_predection(image, nets, Lables, Colors)
+    res, labels = get_predection(image, nets, Lables, Colors)
     # image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     # show the output image
     # cv2.imshow(request.files["output"].read(), image)
@@ -406,17 +492,18 @@ def test_files():
     img_encoded = image_to_byte_array(np_img)
     return Response(response=img_encoded, status=200, mimetype="image/jpeg")
 
-@app.route('/api/test', methods=['POST'])
+
+@ app.route('/api/test', methods=['POST'])
 def main():
     # load our input image and grab its spatial dimensions
-    #image = cv2.imread("./test1.jpg")
+    # image = cv2.imread("./test1.jpg")
 
     img = request.files["image"].read()
     img = Image.open(io.BytesIO(img))
     npimg = np.array(img)
     image = npimg.copy()
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    res,labels = get_predection(image, nets, Lables, Colors)
+    res, labels = get_predection(image, nets, Lables, Colors)
     # image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     # show the output image
     # cv2.imshow(request.files["output"].read(), image)
@@ -425,8 +512,6 @@ def main():
     np_img = Image.fromarray(image)
     img_encoded = image_to_byte_array(np_img)
     return Response(response=img_encoded, status=200, mimetype="image/jpeg")
-
-
 
 
     # start flask app
