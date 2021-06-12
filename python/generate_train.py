5fffde568249d40594dc58a14388ac3f2d2e718e
Jiyeon Jeong <48276522+jiyeon1@users.noreply.github.com>
2021-06-12 21:47:13 +0900

    add ipynb file

---
+++ python/generate_train.py
@@ -0,0 +1,15 @@
+import os
+
+image_files = []
+os.chdir("test")
+# os.chdir(os.path.join("test"))
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith(".jpeg"):
+        image_files.append("data/test/" + filename)
+os.chdir("..")
+with open("test.txt", "w") as outfile:
+    for image in image_files:
+        outfile.write(image)
+        outfile.write("\n")
+    outfile.close()
+os.chdir("..")
